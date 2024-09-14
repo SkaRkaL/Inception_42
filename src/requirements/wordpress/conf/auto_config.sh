f9a028fcdc7e027f469ed2b64cde3bf6a6a409fe
Saad Karkal <karkalsaad@gmail.com>
2024-09-14 23:19:09 +0100

    Inception : Adding www.conf file for WordPress port 9000, and updating script file

--- src/requirements/wordpress/conf/auto_config.sh
+++ src/requirements/wordpress/conf/auto_config.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-sleep 5
+sleep 10
+if [ -f /var/www/wordpress/wp-config.php ]; then
+    echo "wordpress already installed"
+else
+	mkdir -p /var/www/wordpress
 
-if [ ! -f /var/www/wordpress/wp-config.php ]; then
+	cd /var/www/wordpress
 
-    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar  
-    chmod +x wp-cli.phar
-    mv wp-cli.phar /usr/local/bin/wp
 
-    cd /var/www/wordpress
+	rm -rf *
 
-    chmod -R 755 /var/www/wordpress/
-    chown -R root:root /var/www/wordpress
-    chown -R www-data:www-data /var/www/wordpress
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar  
 
-    wp core download --allow-root
+	chmod +x wp-cli.phar
 
-    mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
+	mv wp-cli.phar /usr/local/bin/wp
 
-    sed -i '36 s/\/run\/php\/php7.4-fpm.sock/9000/' /etc/php/7.4/fpm/pool.d/www.conf
+	chmod -R 777 /var/www/wordpress/
 
-    wp config set --allow-root DB_NAME ${MYSQLDB}
-    wp config set --allow-root DB_USER ${MSQLUSER}
-    wp config set --allow-root DB_PASSWORD ${MYSQLPASSWORD}
-    wp config set --allow-root DB_HOST "mariadb:3306"
+	wp core download --allow-root
 
-    wp core install --url=$W_DN --title=$W_TITLE --admin_user=$W_A_N --admin_password=$W_A_P --admin_email=$W_E_A --allow-root 
+	mv /var/www/wordpress/wp-config-sample.php  /var/www/wordpress/wp-config.php
 
-    wp user create ${N_W_USER} ${N_W_EMAIL} --user_pass=$N_W_PASS --role=$N_W_ROLE --allow-root
+	wp config set --allow-root DB_NAME ${MYSQLDB} 
+	wp config set --allow-root DB_USER ${MSQLUSER}
+	wp config set --allow-root DB_PASSWORD ${MYSQLPASSWORD}
+	wp config set --allow-root DB_HOST "mariadb:3306"
+
+	wp core install --url=$W_DN --title=$W_TITLE --admin_user=$W_A_N --admin_password=$W_A_P --admin_email=$W_E_A --allow-root 
+
+	wp user create ${N_W_USER} ${N_W_EMAIL} --user_pass=$N_W_PASS --role=$N_W_ROLE --allow-root
 
 fi
-
-mkdir -p /run/php
-
-/usr/sbin/php-fpm7.4 -F
+exec "$@"

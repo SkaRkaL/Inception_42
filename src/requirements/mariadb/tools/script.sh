044f4d8a2b4b86279f456ad8876f5602e91233b0
Saad Karkal <karkalsaad@gmail.com>
2024-09-14 23:17:04 +0100

    Inception : Fixing confg file and updating Dockerfile commands

--- src/requirements/mariadb/tools/script.sh
+++ src/requirements/mariadb/tools/script.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
+sed -i "s|# port = 3306|port = 3306|1" /etc/mysql/mariadb.cnf
 
 service mariadb start
+	mysqladmin -u root password "${MYSQLROOTPASSWORD}"
+	mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQLDB}\`;"
+	mariadb -e "CREATE USER IF NOT EXISTS \`${MSQLUSER}\`@'%' IDENTIFIED BY '${MYSQLPASSWORD}';"
+	mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQLDB}.* TO \`${MSQLUSER}\`@'%' IDENTIFIED BY '${MYSQLPASSWORD}' ;"
+	mariadb -e "FLUSH PRIVILEGES;"
+service mariadb stop
 
-sleep 5
-
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQLDB}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MSQLUSER}\`@'%' IDENTIFIED BY '${MYSQLPASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON ${MYSQLDB}.* TO \`${MSQLUSER}\`@'%';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQLROOTPASSWORD}';"
-mysql -u root -p$MYSQLROOTPASSWORD -e "FLUSH PRIVILEGES;"
-
-mysqladmin -u root -p$MYSQLROOTPASSWORD shutdown
 
 exec mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql' 
